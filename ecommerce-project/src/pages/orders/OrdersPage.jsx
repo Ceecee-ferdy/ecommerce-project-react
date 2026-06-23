3ba5a461d50721734e5b3a6a99a076f74e555fec
Ceecee-ferdy <confidenceferdinand913@gmail.com>
2026-06-23 13:05:24 +0100

    Seperate into smaller components v1

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -2,9 +2,9 @@ import axios from "axios";
 import { useEffect, useState, Fragment } from "react";
 import dayjs from "dayjs";
 import { Link } from "react-router";
-import { Header } from "../components/Header";
-import BuyAgainIcon from "../assets/images/icons/buy-again.png";
-import { formatMoney } from "../utils/money";
+import { Header } from "../../components/Header";
+import BuyAgainIcon from "../../assets/images/icons/buy-again.png";
+import { formatMoney } from "../../utils/money";
 import "./OrdersPage.css";
 
 export function OrdersPage({ cart }) {
