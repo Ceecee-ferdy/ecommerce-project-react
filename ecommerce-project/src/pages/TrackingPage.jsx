c1744acb96561d190b14f5d00c2519efc4ce0be4
Ceecee-ferdy <confidenceferdinand913@gmail.com>
2026-06-24 12:05:57 +0100

    seperate into smaller components v2

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,14 +1,30 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router'
 import { Header } from '../components/Header'
 import './TrackingPage.css'
 
 
-export function TrackingPage() {
+export function TrackingPage({ cart }) {
+  const { orderId, productId} = useParams();
+
+  const [ order, setOrder ] = useState(null);
+
+  useEffect(() => {
+    const fetchTrackingData = async () => {
+      const response = await axios.get(`/api/orders/${orderId}?expand=products`);
+      setOrder(response.data);
+    }
+      fetchTrackingData()
+    
+  }, [orderId])
+  
   return(
     <>
     <title>Tracking</title>
     <link rel="icon" type="image/svg+xml" href="tracking-favicon.png" />
 
-     <Header />
+     <Header cart={cart}/>
 
     <div className="tracking-page">
       <div className="order-tracking">
